26e2f67992051703f0dab874ae8baf5220ccff36
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-07-10 16:23:18 +0530

    Optimized code

--- app.js
+++ app.js
@@ -2,40 +2,73 @@ import { createServer } from "http";
 import { readFile } from "fs/promises";
 import path from "path";
 
-const server = createServer(async (req, res) =>{
-    if(req.method === "GET"){
-        if(req.url === "/"){
-            try {
-                const data = await readFile(path.join("public","index.html"));
-                res.writeHead(200,{"Content-Type": "text/html"});
-                res.end(data);
-            } catch (error) {
-                res.writeHead(404,{"Content-Type":"text/html"});
-                res.end("404 page not found");
-            }
-        }else if(req.method === "GET"){
-        if(req.url === "/style.css"){
-            try {
-                const data = await readFile(path.join("public","style.css"));
-                res.writeHead(200,{"Content-Type": "text/css"});
-                res.end(data);
-            } catch (error) {
-                res.writeHead(404,{"Content-Type":"text/html"});
-                res.end("404 page not found");
-            }
-        }
-        else {
-            res.writeHead(404, { "Content-Type": "text/plain" });
-            res.end("404 - Page Not Found");
+//use a function for without repetation
+const serveFile = async (res, filePath, contentType) => {
+    try {
+        const data = await readFile(filePath);
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+    } catch (error) {
+        res.writeHead(404, { "Content-Type": content / plain });
+        res.end("404 page not found");
     }
-}else {
-        res.writeHead(405, { "Content-Type": "text/plain" });
-        res.end("405 - Method Not Allowed");
-}
+};
+
+const server = createServer((req, res) => {
+    if (req.method === "GET") {
+        if (req.url === "/") {
+            return serveFile(res, path.join("public", "index.html"), "text/html");
+        } else if (req.method === "GET") {
+            if (req.url === "/style.css") {
+                return serveFile(res, path.join("public", "style.css"), "text/css");
+            }
+            else {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.end("404 - Page Not Found");
+            }
+        } else {
+            res.writeHead(405, { "Content-Type": "text/plain" });
+            res.end("405 - Method Not Allowed");
+        }
     }
 });
 
+//here repetation shown 
+
+// const server = createServer(async (req, res) =>{
+//     if(req.method === "GET"){
+//         if(req.url === "/"){
+//             try {
+//                 const data = await readFile(path.join("public","index.html"));
+//                 res.writeHead(200,{"Content-Type": "text/html"});
+//                 res.end(data);
+//             } catch (error) {
+//                 res.writeHead(404,{"Content-Type":"text/html"});
+//                 res.end("404 page not found");
+//             }
+//         }else if(req.method === "GET"){
+//         if(req.url === "/style.css"){
+//             try {
+//                 const data = await readFile(path.join("public","style.css"));
+//                 res.writeHead(200,{"Content-Type": "text/css"});
+//                 res.end(data);
+//             } catch (error) {
+//                 res.writeHead(404,{"Content-Type":"text/html"});
+//                 res.end("404 page not found");
+//             }
+//         }
+//         else {
+//             res.writeHead(404, { "Content-Type": "text/plain" });
+//             res.end("404 - Page Not Found");
+//     }
+// }else {
+//         res.writeHead(405, { "Content-Type": "text/plain" });
+//         res.end("405 - Method Not Allowed");
+// }
+//     }
+// });
+
 const PORT = 3002;
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`server running at https://localhost:${PORT}`);
 });
