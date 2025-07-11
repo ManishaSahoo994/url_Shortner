06ef8723cc41534acbb61ebfbac24572ba2d9248
Manisha Sahoo <manishasahoo2343@gmail.com>
2025-07-11 11:31:05 +0530

    Load links completed

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import { createServer } from "http";
 import { readFile } from "fs/promises";
+import { writeFile } from "fs/promises";
+import crypto from "crypto";
 import path from "path";
+import { writeFile } from "fs";
 
+
+const DATA_FILE = path.join("data", "links.json");
 //use a function for without repetation
 const serveFile = async (res, filePath, contentType) => {
     try {
@@ -9,17 +14,29 @@ const serveFile = async (res, filePath, contentType) => {
         res.writeHead(200, { "Content-Type": contentType });
         res.end(data);
     } catch (error) {
-        res.writeHead(404, { "Content-Type": content / plain });
+        res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("404 page not found");
     }
 };
 
-const server = createServer((req, res) => {
+const loadLinks = async () =>{
+    try {
+        const data = await readFile(DATA_FILE, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        if(error.code === "ENOENT"){
+        await writeFile(DATA_FILE, JSON.stringify({}));
+        return {};
+        }
+        throw error;
+    }
+};
+
+const server = createServer(async(req, res) => {
     if (req.method === "GET") {
         if (req.url === "/") {
             return serveFile(res, path.join("public", "index.html"), "text/html");
-        } else if (req.method === "GET") {
-            if (req.url === "/style.css") {
+        } else if (req.url === "/style.css") {
                 return serveFile(res, path.join("public", "style.css"), "text/css");
             }
             else {
@@ -30,8 +47,33 @@ const server = createServer((req, res) => {
             res.writeHead(405, { "Content-Type": "text/plain" });
             res.end("405 - Method Not Allowed");
         }
-    }
-});
+
+        if(req.method === "POST" && req.url === "/shorten"){
+
+            const links = await loadLinks();
+
+            let body="";
+            req.on("data", (chunk)=>{
+                body = body+chunk;
+            });
+            req.on("end", ()=>{
+                console.log(body);
+                const {url, shortCode} = JSON.parse(body);
+
+                if(!url){
+                    res.writeHead(400, {"Content-Type": "text/plain"});
+                    return res.end("URL is required");
+                }
+                const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+                if(links[finalShortCode]){
+                    res.writeHead(400, {"Content-Type": "text/plane"});
+                    return res.end("Short code already exists. Please choose another.");
+                }
+
+            });
+        }
+    });
 
 //here repetation shown 
 
